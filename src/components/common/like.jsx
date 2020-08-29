f90ebd34ebb7970a6e6fd05084977f714859f9a8
sumonsheikh <sm.sumon117@gmail.com>
2020-08-29 14:22:54 +0600

    composing like component

---
+++ src/components/common/like.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Like extends Component {
+    state = {  }
+    render() { 
+        return (<i className="fa fa-heart-o"></i>  );
+    }
+}
+ 
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Like from './common/like';
 import {getMovies} from '../services/fakeMovieServices';
 class Movies extends Component {
     state = {
@@ -17,11 +18,12 @@ class Movies extends Component {
                 <table className="table">
                 <thead>
                     <tr>
-                        <td>Title</td>
-                        <td>Genre</td>
-                        <td>Stock</td>
-                        <td>Rate</td>
-                        <td></td>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                        <th></th>
+                        <th/>
                     </tr>
                 </thead>
                 <tbody>
@@ -31,6 +33,9 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like/>
+                            </td>
                             <td><button onClick={()=>this.handaleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
                     ))}
